3e99ca12ee055abb38d5e22118cd0f70e1e8d9fb
sergey1369 <sergey4spam@gmail.com>
2022-10-31 13:33:50 +0300

    Notify me added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() product!: Product;
+  @Output() notify = new EventEmitter();
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
